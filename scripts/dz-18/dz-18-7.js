ebd3304c1c0e412fc210a8dea3931fc04752271b
Skyrtach Vitalii <vitalijskirtac@gmail.com>
2021-01-27 23:23:09 +0200

    git add *

--- scripts/dz-18/dz-18-7.js
+++ scripts/dz-18/dz-18-7.js
@@ -1,13 +1,42 @@
-//Лотерея
-let number1 = prompt("Введіть число від якого відмірюється діапазон чисел: ");
-let number2 = prompt("Введіть число до якого відмірюється діапазон чисел: ");
+//Лотарея
 
+let arrNums = [];
+let randomArr = [];
 
-function numberRandom(number1, number2) {
-  for(let i = 0; i < 10; i++){
-  let rand = Math.floor(number1 + Math.random() * (number2 + 1 - number1));
-  console.log(rand);
+function getNums(){
+  for(let i = 0; i < 6; i++){
+    let num = prompt("Введіть число від 1 до 36: ");
+    arrNums.push(num);
   }
+  console.log("Числа які ви ввели: " + arrNums.join());
 }
-  
-numberRandom(Number(number1), Number(number2));
+
+function getRandomInRange() {
+  for(let i = 0; i < 6; i++){
+  let randomNum = Math.floor(Math.random() * (36 - 1 + 1)) + 1;
+  randomArr.push(randomNum);
+  }
+  console.log("Згенерований массив: " + randomArr);
+}
+
+function checkArrs(arrNums, randomArr){
+  let messageArr = "";
+  let result = 0;
+  for(let i = 0; i < arrNums.length; i++){
+      if (arrNums[i] == randomArr[i]){
+        messageArr += arrNums[i] + "-yes ";
+        result++;
+      }
+      else{
+        messageArr += arrNums[i] + "-no ";
+      }
+  }
+console.log(messageArr);
+console.log("Результат:" + result + "/6")
+}
+
+getNums();
+console.log("-------------");
+getRandomInRange();
+console.log("-------------");
+checkArrs(arrNums, randomArr);
