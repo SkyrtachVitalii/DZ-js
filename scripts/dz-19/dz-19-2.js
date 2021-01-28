015e27576d415b79f6c17f36732999fc613883b3
Skyrtach Vitalii <vitalijskirtac@gmail.com>
2021-01-28 19:07:28 +0200

    create js for DZ PowerCode

--- scripts/dz-19/dz-19-2.js
+++ scripts/dz-19/dz-19-2.js
@@ -1,21 +1,21 @@
-let arrNums = [34, 15, 8, 5, 56, 50, 30, 25, 80, 13, 16, 108];
+let arrNumsS = [34, 15, 8, 5, 56, 50, 30, 25, 80, 13, 16, 108];
 let nNums = prompt("Введіть кількість перевірюваних чисел з масису: ");
 Number.nNums;
 
-function checkArr(arrNums, nNums){
-  for(let i = 0; i < nNums; i++){
-    arrNums.splice(nNums-1, arrNums.length);
-    if((arrNums[i] % 3 === 0) && (arrNums[i] % 5 === 0)){
-      arrNums[i] = "fizzbuzz";
+function checkArr(arrNumsS, nNums){
+  for(let i = 0; i < arrNumsS; i++){
+    arrNumsS.splice(arrNumsS-1, arrNumsS.length);
+    if((arrNumsS[i] % 3 === 0) && (arrNumsS[i] % 5 === 0)){
+      arrNumsS[i] = "fizzbuzz";
     }
-    else if((arrNums[i] % 3 === 0) && (arrNums[i] % 5 !== 0)){
-      arrNums[i] = "fizz";
+    else if((arrNumsS[i] % 3 === 0) && (arrNumsS[i] % 5 !== 0)){
+      arrNumsS[i] = "fizz";
     }
     else{
-      arrNums[i] = "buzz";
+      arrNumsS[i] = "buzz";
     }
   }
 }
 
-checkArr(arrNums, nNums);
-console.log(arrNums);
+checkArr(arrNumsS, nNums);
+console.log(arrNumsS);
