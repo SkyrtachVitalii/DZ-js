59cb1fef13a2ebf4c28967ccf7e3ab705c1a07b9
Skyrtach Vitalii <vitalijskirtac@gmail.com>
2021-01-28 20:33:55 +0200

    create js for DZ PowerCode

--- scripts/dz-19/dz-19-2.js
+++ scripts/dz-19/dz-19-2.js
@@ -1,10 +1,10 @@
 let arrNumsS = [34, 15, 8, 5, 56, 50, 30, 25, 80, 13, 16, 108];
-let nNums = prompt("Введіть кількість перевірюваних чисел з масису: ");
+let nNums = prompt("Введіть кількість перевірюваних чисел з масиву: ");
 Number.nNums;
 
 function checkArr(arrNumsS, nNums){
-  for(let i = 0; i < arrNumsS; i++){
-    arrNumsS.splice(arrNumsS-1, arrNumsS.length);
+  for(let i = 0; i < nNums; i++){
+    arrNumsS.splice(nNums-1, arrNumsS.length);
     if((arrNumsS[i] % 3 === 0) && (arrNumsS[i] % 5 === 0)){
       arrNumsS[i] = "fizzbuzz";
     }
