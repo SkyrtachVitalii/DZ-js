dc7e207d5a0261a909e3f2f545ba7786d7111532
Skyrtach Vitalii <vitalijskirtac@gmail.com>
2021-02-22 17:38:00 +0200

    first commit

---
+++ scripts/dz-25/dz-25-2.js
@@ -0,0 +1,39 @@
+let products = [
+    {
+        name: 'Apple iPhone 12',
+        price: 1000,
+        currency: 'USD',
+        status: 'active'
+    },
+    {
+        name: 'Xiaomi Redmi Note 9',
+        price: 600,
+        currency: 'USD',
+        status: 'active'
+    },
+    {
+        name: 'Samsung Galaxy s20',
+        price: 850,
+        currency: 'USD',
+        status: active
+    },
+    {
+        name: 'Apple iPhone X',
+        price: 550,
+        currency: 'USD',
+        status: 'active'
+    },
+    {
+        name: 'Xiaomi Redmi 5',
+        price: 200,
+        currency: 'USD',
+        status: 'not_available'
+    },
+    {
+        name: 'Samsung Note 8',
+        price: 400,
+        currency: 'USD',
+        status: 'not_active'
+    },
+]
+    
